18e83fdb1cde9b431e662352042413e9e67fca7e
pjy <wodus331@vanillacode.net>
2023-10-06 10:12:51 +0900

    Toaster add...

--- src/App.jsx
+++ src/App.jsx
@@ -54,6 +54,8 @@ function App(props) {
                          },style:{
                              fontSize: '16px',
                              maxWidth:'500px',
+                             padding: '16px 24px',
+                             backgroundColor: 'var(--color-grey-0)',
                          }
                      }}
             />
