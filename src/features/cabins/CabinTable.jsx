b001fea75cd834d04360d4e38c1994b1dfda991c
pjy <wodus331@vanillacode.net>
2023-10-08 03:39:29 +0900

    Render Patten -> Table.Body

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -36,7 +36,10 @@ function CabinTable(props) {
                 <div>Discount</div>
                 <div></div>
             </Table.Header>
-            {cabins.map(cabin => <CabinRow cabin={cabin} key={cabin.id}/>)}
+            <Table.Body data={cabins}
+                        render={cabin => <CabinRow cabin={cabin}
+                                                   key={cabin.id}/>}
+            />
         </Table>
     );
 }
