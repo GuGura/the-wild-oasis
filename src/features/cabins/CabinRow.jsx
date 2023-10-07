e0fe09d168ff46b433e7b95b0f02dfd984dd12e4
pjy <wodus331@vanillacode.net>
2023-10-08 03:33:37 +0900

    Compound Component -> change table

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -8,18 +8,19 @@ import {HiPencil, HiSquare2Stack, HiTrash} from "react-icons/hi2";
 import {useCreateCabin} from "./useCreateCabin.js";
 import Modal from "../../ui/Modal.jsx";
 import ConfirmDelete from "../../ui/ConfirmDelete.jsx";
+import Table from "../../ui/Table.jsx";
 
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
+// const TableRow = styled.div`
+//   display: grid;
+//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//   column-gap: 2.4rem;
+//   align-items: center;
+//   padding: 1.4rem 2.4rem;
+//
+//   &:not(:last-child) {
+//     border-bottom: 1px solid var(--color-grey-100);
+//   }
+// `;
 
 const Img = styled.img`
   display: block;
@@ -74,7 +75,7 @@ function CabinRow({cabin}) {
     }
 
     return (
-        <TableRow role='row'>
+        <Table.Row>
             <Img src={image}/>
             <Cabin>{name}</Cabin>
             <div>Fits up to {maxCapacity}</div>
@@ -92,17 +93,17 @@ function CabinRow({cabin}) {
                     <Modal.Window name='edit'>
                         <CreateCabinForm cabinToEdit={cabin}/>
                     </Modal.Window>
-                    <Modal.Open>
+                    <Modal.Open opens='delete'>
                         <button><HiTrash/></button>
                     </Modal.Open>
-                    <Modal.Window>
+                    <Modal.Window name='delete'>
                         <ConfirmDelete disabled={isDeleting}
                                        onConfirm={() => deleteCabin(cabinId)}
                                        resourceName='cabins'/>
                     </Modal.Window>
                 </Modal>
             </div>
-        </TableRow>
+        </Table.Row>
     );
 }
 
