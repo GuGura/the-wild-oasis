ad6f2a8c45bf216fb4a6fc21fd3e6132da5e9c77
pjy <wodus331@vanillacode.net>
2023-10-11 22:21:35 +0900

    fix

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -71,11 +71,12 @@ export async function updateCurrentUser({password, fullName, avatar}) {
     if (storageError) throw new Error(storageError.message)
 
     // 3. Update avatar in the user
-    const {data: updateUser, error: error2} = supabase.auth.updateUser({
-        data: {
-            avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`
-        }
-    })
+    const {data: updateUser, error: error2} =
+        await supabase.auth.updateUser({
+            data: {
+                avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`
+            }
+        })
     if (error2) throw new Error(error2.message)
 
     return updateUser
